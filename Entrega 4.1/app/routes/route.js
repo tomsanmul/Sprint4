9c2bfc1fba6aaa140052c2a5521c53407e9b7337
Tomas Sans <tomsan@gmail.com>
2022-11-30 13:06:32 +0100

    Canvis

--- Entrega 4.1/app/routes/route.js
+++ Entrega 4.1/app/routes/route.js
@@ -5,9 +5,9 @@ const app = require("../config/config.js");
 app.get('/', (req, res) => {
     res.send({
         "Llista d'EndPoints:": "",
-        "/user": "retorna un json amb el teu nom, edat i l'URL des d'on es fa la petició.",
-        "/upload": "puja al servidor un arxiu de tipus png/jpg/gif amb una petició POST i que retorni un missatge d'error en cas que l'extensió de l'arxiu no coincideixi amb aquestes.",
-        "/time": "rep per POST com a paràmetre un JSON amb el nom d'usuari i retorna un objecte JSON que conté l'hora i data actual. Inclou un middleware que afegeix la capçalera Cache-control: no-cache. Habilita CORS en les respostes mitjançant Express"
+        "/user": "GET -> retorna un JSON amb el teu nom, edat i l'URL des d'on es fa la petició.",
+        "/upload": "POST -> puja al servidor un arxiu de tipus png/jpg/gif amb una petició POST i que retorni un missatge d'error en cas que l'extensió de l'arxiu no coincideixi amb aquestes.",
+        "/time": "POST -> rep per POST com a paràmetre un JSON amb el nom d'usuari i retorna un objecte JSON que conté l'hora i data actual. Inclou un middleware que afegeix la capçalera Cache-control: no-cache. Habilita CORS en les respostes mitjançant Express"
     })
 });
 
@@ -72,7 +72,8 @@ app.post('/upload', async (req, res) => {
 //app.post('/time', cors(), async (req, res) => {
 app.post('/time', async (req, res) => {
     try {
-        if (!req.files) {
+        console.log(req.params);
+        if (!req.body) {
             res.send({
                 status: false,
                 message: 'No file JSON received'
