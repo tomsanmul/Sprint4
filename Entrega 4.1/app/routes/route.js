a0abf7489936572f78fece26e2abe94546f83843
tomsanmul <tomsan@gmail.com>
2023-01-25 19:17:41 +0100

    Canvis

--- Entrega 4.1/app/routes/route.js
+++ Entrega 4.1/app/routes/route.js
@@ -9,14 +9,14 @@ app.get('/', (req, res) => {
         "/user": "GET -> retorna un JSON amb el teu nom, edat i l'URL des d'on es fa la petició.",
         "/upload": "POST -> puja al servidor un arxiu de tipus png/jpg/gif amb una petició POST i que retorni un missatge d'error en cas que l'extensió de l'arxiu no coincideixi amb aquestes.",
         "/time": "POST -> rep per POST com a paràmetre un JSON amb el nom d'usuari i retorna un objecte JSON que conté l'hora i data actual. Inclou un middleware que afegeix la capçalera Cache-control: no-cache. Habilita CORS en les respostes mitjançant Express",
-        "/pokemon": "GET -> retorna un JSON amb el nom del Pokemon, pes i alçada. "
+        "/pokemon": "GET -> retorna un JSON amb el nom del Pokemon, alçada i el pes. "
     })
 });
 
 
 app.get('/user', (req, res) => {
 
-     controller.getUser(req,res);
+    controller.getUser(req, res);
 
 
 });
@@ -24,20 +24,20 @@ app.get('/user', (req, res) => {
 
 app.post('/upload', async (req, res) => {
 
-    controller.upload(req,res);
+    controller.upload(req, res);
 
 });
 
 
 app.post('/time', async (req, res) => {
 
-    controller.time(req,res);
+    controller.time(req, res);
 
 });
 
 app.get('/pokemon/:id', async (req, res) => {
 
-    await controller.getPokemon(req,res);
+    await controller.getPokemon(req, res);
 
 });
 
